d4cc5f69b75da5a6b26ef7d1f3e251b04e492455
Jana Kunft <jana.kunft@and.digital>
2023-08-02 18:43:18 +0100

    initial setup completed

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import postcss from 'rollup-plugin-postcss';
+
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
@@ -20,11 +25,19 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser()
+    ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/, 'react-dom']
   }
 ];
